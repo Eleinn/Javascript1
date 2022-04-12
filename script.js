ff4efdf6fd0d6c53128bf91c19d655c213f93a0f
Eleinn <elenaescandarani@gmail.com>
2022-04-12 14:25:51 -0300

    Clase Javascript

---
+++ script.js
@@ -0,0 +1,33 @@
+//acá meto un popup
+
+alert("Hola Mundo")
+
+//Strings
+let str1 =  "String con comillas dobles "
+let str2 = 'String con comillas simples'
+let str3 = `String con Backticks`
+
+let str5 = `${str1} y ademas ${str2} `
+let str6 = str1 + " y ademas " + str2;
+
+console.log(str6)
+
+//Numeros
+let nombre = prompt("Cómo te llamás?")
+let numero1 = parseFloat(prompt("Ingresá el 1º número"));
+let numero2 = parseInt(prompt ("Ingresá el 2º número"));
+let multiplicacion = numero1*numero2
+let suma = numero1 + numero2
+let resta = numero1-numero2
+
+alert("Hola " + nombre)
+alert("La multiplicación es: " + multiplicacion)
+alert("La suma es: " + suma)
+alert("La resta es: " + resta)
+
+//Booleanos
+//let verdadera = true
+//let falsa = false
+
+//let respuesta = 3>4
+//console.log(respuesta)
